fac3e4b89a2b805a938663dda963b9e7de7df3c3
rmamchyk <roman.mamchyk@gmail.com>
2018-07-16 19:09:31 +0300

    Add POST /users route

--- server/server.js
+++ server/server.js
@@ -97,6 +97,17 @@ app.patch('/todos/:id', (req, res)=>{
   });
 });
 
+// POST /users
+app.post('/users', (req, res)=>{
+  var user = new User(_.pick(req.body, ['email', 'password']));
+
+  user.save().then((doc)=>{
+    res.send(doc);
+  }, (err)=>{
+    res.status(400).send(err);
+  })
+});
+
 app.listen(port, ()=>{
   console.log(`Started up at port ${port}`);
 });
